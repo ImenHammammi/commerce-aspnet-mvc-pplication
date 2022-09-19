4999647fef838291f194ba63302ab5bd3ec059fa
Imen <Imen@DESKTOP-IMEN>
2022-09-19 08:18:42 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace commerceApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
